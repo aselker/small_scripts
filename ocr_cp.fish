6307705c0d21e0ac3f5008b5729c6d9806f3d68b
Adam Selker <adamselker@gmail.com>
2020-04-28 14:17:26 -0700

    Copy, don't symlink

--- ocr_cp.fish
+++ ocr_cp.fish
@@ -1,1 +1,6 @@
-/usr/bin/ocr_cp
+#!/usr/bin/env fish
+
+set image_name (mktemp --suffix=.png)
+import $image_name
+tesseract -c page_separator='' $image_name stdout | xclip -sel clip
+rm $image_name
--- toggletouchscreen.fish
+++ toggletouchscreen.fish
@@ -1,1 +1,7 @@
-/usr/bin/toggletouchscreen
+#!/usr/bin/env fish
+
+if xinput list-props "ELAN Touchscreen" | grep -q 'Device Enabled.*:.*1$'
+	xinput disable "ELAN Touchscreen"
+else if xinput list-props "ELAN Touchscreen" | grep -q 'Device Enabled.*:.*0$'
+	xinput enable "ELAN Touchscreen"
+end
