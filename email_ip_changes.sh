5acd6acb3f7719b8418e209796771f9a63423abe
Adam Selker <adam.selker@joby.aero>
2021-11-19 14:07:25 -0800

    Survive internet loss? and a little neater

--- email_ip_changes.sh
+++ email_ip_changes.sh
@@ -4,14 +4,14 @@ OLD_IP="/home/adam.selker/Temp/ip_addr"
 
  if [ -f $OLD_IP ]
  then
-	 if [ `cat $OLD_IP` = `ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+'` ]
+	 NEW_IP=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
+	 if [ `cat $OLD_IP` = $NEW_IP ]
 	 then
 		  #echo "no change in IP."
 		  true
 	 else
-		  ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+' > $OLD_IP
-		  echo "Subject: $(hostname) now $(cat $OLD_IP)" | sendmail adamselker@gmail.com
+		  echo "Subject: $(hostname) now $NEW_IP" | /usr/sbin/sendmail adamselker@gmail.com && echo $NEW_IP > $OLD_IP
 	  fi
  else
-	  ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+' > $OLD_IP
+	  echo $NEW_IP > $OLD_IP
  fi
