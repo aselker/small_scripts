b110e038f28cb251e7c6a6494cc94fb79e6d01d9
Adam Selker <adamselker@gmail.com>
2022-08-11 20:16:26 -0700

    Damn I'm good

--- cmd_history_merger.py
+++ cmd_history_merger.py
@@ -3,10 +3,39 @@ import datetime
 import dateutil.parser
 import numpy as np
 
+
 def parse_time(line):
     time_str = line.split("|")[1]
-    time = dateutil.parser.parse(time_str)# , "%a %b %d %X %Z %Y")
+    time = dateutil.parser.parse(time_str)  # , "%a %b %d %X %Z %Y")
     return time
+
+
+def parse(lines):
+    lines_out = []
+    i = 0
+    while True:
+        line = lines[i]
+        try:
+            pipe_parts = line.split("|")
+            if pipe_parts[0] not in ["triton", "tdl-8lcr333", "lxl-cjzqn13"]:
+                is_continuation = True
+            else:
+                try:
+                    time = parse_time(line)
+                    is_continuation = False
+                except dateutil.parser._parser.ParserError:
+                    is_continuation = True
+        except IndexError:
+            is_continuation = True
+
+        if is_continuation:
+            lines_out[-1][1] = lines_out[-1][1] + line
+        else:
+            lines_out.append([time, line])
+
+        i += 1
+        if i == len(lines):
+            return lines_out
 
 
 forever = datetime.datetime(datetime.MAXYEAR, 12, 31, tzinfo=datetime.timezone.utc)
@@ -15,9 +44,10 @@ files = [open(filename, "r") for filename in sys.argv[1:]]
 liness = [file.readlines() for file in files]
 [file.close() for file in files]
 
+commandss = [parse(lines) for lines in liness]
+
 indices = [0 for _ in files]
-lines = [lines_[0] for lines_ in liness]
-times = [parse_time(line) for line in lines]
+times = [commands[0][0] for commands in commandss]
 
 output = []
 
@@ -27,15 +57,16 @@ while True:
     if time == forever:
         break
     else:
-        output.append(lines[argmin])
+        output.append(commandss[argmin][indices[argmin]][1])
         for i in range(len(indices)):
             if times[i] == time:
                 indices[i] += 1
-                if indices[i] == len(liness[i]):
+                if indices[i] == len(commandss[i]):
                     times[i] = forever
                 else:
-                    lines[i] = liness[i][indices[i]]
-                    times[i] = parse_time(lines[i])
+                    times[i] = commandss[i][indices[i]][0]
 
+assert output[-1][-1] == "\n"
+output[-1] = output[-1][:-1]
 
-print(''.join(output))
+print("".join(output))
