92ae98ddd873fa2d1c0663709c041d97dfa9dc1f
Adam Selker <adam.selker@joby.aero>
2023-02-28 13:53:40 -0800

    eeEEEEeee

--- ddc_brightness.sh
+++ ddc_brightness.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ddcutil setvcp 10 $1 -d 1
-ddcutil setvcp 10 $1 -d 2
-ddcutil setvcp 10 $1 -d 3
+ddcutil setvcp 10 $(echo "$1 * 1.7" | bc | awk '{printf("%d\n",$1 + 0.5)}') -d 2
+#ddcutil setvcp 10 $1 -d 3
