31688a686251217fe25e1d534cdbfeae23223ec7
Adam Selker <adamselker@gmail.com>
2023-07-26 15:59:44 -0700

    no way

--- ocr_cp.fish
+++ ocr_cp.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
 set image_name (mktemp --suffix=.png)
-import $image_name
-tesseract -c page_separator='' $image_name stdout | xclip -sel clip
+# import $image_name
+# tesseract -c page_separator='' $image_name stdout | xclip -sel clip
+grimshot save area $image_name
+tesseract -c page_separator='' $image_name stdout | wl-copy
 rm $image_name
---
+++ sway_flip_screen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if swaymsg -t get_outputs | grep -q "normal"
+then
+	swaymsg output eDP-1 transform 180
+else
+	swaymsg output eDP-1 transform normal
+fi
+
+swaymsg input 1386:20896:Wacom_Pen_and_multitouch_sensor_Pen map_to_output eDP-1
+swaymsg input 1386:20896:Wacom_Pen_and_multitouch_sensor_Finger map_to_output eDP-1
+#swaymsg input 2:10:TPPS/2_Elan_TrackPoint map_to_output eDP-1
+#swaymsg input 2:7:SynPS/2_Synaptics_TouchPad map_to_output eDP-1
