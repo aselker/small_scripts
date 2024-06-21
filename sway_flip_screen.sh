820d7abe5cf5b74d5a720bf23d45129cd7d3b2d9
Adam Selker <adamselker@gmail.com>
2024-06-21 10:58:55 -0700

    sway_flip_screen.sh: autodetect output name

--- sway_flip_screen.sh
+++ sway_flip_screen.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# TODO: Autodetect eDP name
+output_name=$(swaymsg -t get_outputs | jq -r '.[]|.name')
 
 if swaymsg -t get_outputs | grep -q "normal"
 then
-	swaymsg output eDP-1 transform 180
+	swaymsg output $output_name transform 180
 else
-	swaymsg output eDP-1 transform normal
+	swaymsg output $output_name transform normal
 fi
 
-swaymsg input 1386:20896:Wacom_Pen_and_multitouch_sensor_Pen map_to_output eDP-1
-swaymsg input 1386:20896:Wacom_Pen_and_multitouch_sensor_Finger map_to_output eDP-1
-#swaymsg input 2:10:TPPS/2_Elan_TrackPoint map_to_output eDP-1
-#swaymsg input 2:7:SynPS/2_Synaptics_TouchPad map_to_output eDP-1
+swaymsg input 1386:20896:Wacom_Pen_and_multitouch_sensor_Pen map_to_output $output_name
+swaymsg input 1386:20896:Wacom_Pen_and_multitouch_sensor_Finger map_to_output $output_name
